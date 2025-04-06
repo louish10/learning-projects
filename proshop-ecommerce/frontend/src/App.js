24daf5ebb19ea32490307a81b405b1d9bebe4df7
Louis <louish10@hotmail.co.uk>
2025-04-06 22:56:33 +0100

    Add router

--- proshop-ecommerce/frontend/src/App.js
+++ proshop-ecommerce/frontend/src/App.js
@@ -1,19 +1,19 @@
 import { Container } from "react-bootstrap";
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import HomeScreen from './screens/HomeScreen'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Header />
-    <main className="py-3">
-      <Container>
-        <HomeScreen/>
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet></Outlet>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
-export default App
+};
+export default App;
--- proshop-ecommerce/frontend/src/components/Header.jsx
+++ proshop-ecommerce/frontend/src/components/Header.jsx
@@ -1,23 +1,23 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
-
+import { Link } from "react-router";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <img src={logo} alt="ProShop" />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
