d2fdf3c994804fc2293679b55e9a852652b72517
Louis <louish10@hotmail.co.uk>
2025-04-07 22:16:22 +0100

    Add backend

--- proshop-ecommerce/frontend/src/screens/ProductScreen.jsx
+++ proshop-ecommerce/frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,19 @@
 import { useParams, Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import products from "../products";
+import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/product/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
