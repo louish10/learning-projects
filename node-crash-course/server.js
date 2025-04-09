7f9081b26fe86b0dbfafee022b5f2e9488dc4dd2
Louis Headley <louish10@hotmail.co.uk>
2025-04-09 13:31:45 +0100

    Add routing

--- node-crash-course/server.js
+++ node-crash-course/server.js
@@ -1,9 +1,17 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(500, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify({message: "Error"}));
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>Home</h1>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>About</h1>');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end('<h1>Not Found</h1>');
+    }
 })
 
-const port = 3000
+const port = process.env.PORT
 server.listen(port, () => { console.log(`Connected to ${port}`) })
