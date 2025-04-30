078b8f1771099f0a6fce12795da742e6bed30c88
Louis Headley <louish10@hotmail.co.uk>
2025-04-30 10:51:52 +0100

    Playing with playground

--- tailwind-playground/index.js
+++ tailwind-playground/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 app.use(express.static('public'))
 app.use('/', (req, res) => {
   res.sendFile(__dirname  + '/views/index.html')
 })
+
 app.listen(3001, console.log("3001"))
